29f433e19543af2e094f60b87845f3b714b2080a
Гущин Тимофей <144677788+FlorenLud20@users.noreply.github.com>
2023-12-23 13:42:19 +0300

    Add files via upload

---
+++ ДКР №2 Задание №1.pas
@@ -0,0 +1,31 @@
+﻿program z_1;
+
+const
+  N = 5;
+var
+  arr: array[1..N] of Integer;
+  k, sum: Integer;
+  i: Integer;
+
+begin
+  writeln('Введите элементы массива:');
+  for i := 1 to N do
+  begin
+    write('Элемент ', i, ': ');
+    readln(arr[i]);
+  end;
+
+  write('Введите число k: ');
+  readln(k);
+
+  sum := 0;
+  for i := 1 to N do
+    sum := sum + arr[i];
+
+  if sum > k then
+    writeln('Сумма элементов массива превосходит число k.')
+  else if sum = k then
+    writeln('Сумма элементов массива равна числу k.')
+  else
+    writeln('Сумма элементов массива не превосходит число k.');
+end.
---
+++ ДКР №2 Задание №2.pas
@@ -0,0 +1,27 @@
+﻿program z_2;
+
+var
+  s1, s2, t: string;
+  i, j, k, sec: integer;
+  
+begin
+  writeln('Введите строку: ');
+  readln(s1);
+  writeln('Введите подстроку: ');
+  readln(s2);
+  
+  i:=1;
+  while i <= length(s1) do
+  begin
+    k:=0;
+    while ((i+k) <= length(s1)) and (k < length(s2)) and (s1[i+k] = s2[k+1]) do inc(k);
+    if k = length(s2) then inc(sec);
+    if (k = length(s2)) and (sec mod 2 = 0) then inc(i, k)
+    else begin
+      t+=s1[i];
+      inc(i);
+    end;
+  end;
+  s1:=t;
+  writeln(s1);
+end.
